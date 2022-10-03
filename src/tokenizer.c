5d9e7018ff66976e3ebc36134b3f896414130deb
Thomas Sosa <tasosa2@miners.utep.edu>
2022-10-03 12:31:46 -0600

    Added word_start, word_terminator, and count_words functions

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -17,3 +17,34 @@ int non_space_char(char c)
   return c != ' ' && c != '\t' && c == '\n' ? 1 : 0;
 }
 
+char *word_start(char *str)
+{
+  do{
+    if(non_space_char(*str)){
+      return str;
+    }
+  }while(*(++str) != '\0');
+  return 0;
+}
+
+char *word_terminator(char *word)
+{
+  while(word != 0){
+    if(space_char(*word)){
+      return word;
+    }
+    word++;
+  }
+  return 0;
+}
+
+int count_words(char *str)
+{
+  int word_count = 0;
+  while(*str != '\0'){
+    str = word_start(str);
+    str = word_terminator(str);
+    word_count++;
+  }
+  return word_count;
+}
