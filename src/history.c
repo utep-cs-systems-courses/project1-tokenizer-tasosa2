89675eb0108bf34b0be3c475dd1a1d2f63dd32e4
Thomas Sosa <tasosa2@miners.utep.edu>
2022-10-04 22:26:50 -0600

    Created history.c and added its required functions (not filled in yet)

---
+++ src/history.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "history.h"
+#include "tokenizer.h"
+
+
+List* init_history()
+{
+
+}
+
+void add_history(List* list, char* str)
+{
+
+}
+
+char *get_history(List* list, int id)
+{
+
+}
+
+void print_history(List* list)
+{
+
+}
+
+void free_history(List* list )
+{
+
+}
