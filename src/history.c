b578d62e366e8d67e68d5b1d12b2cfc6c9b35fec
student <tasosa2@miners.utep.edu>
2022-10-05 18:30:41 -0600

    Added init_History, add_history, and get_history functions

--- src/history.c
+++ src/history.c
@@ -6,17 +6,52 @@
 
 List* init_history()
 {
-
+  List* history;
+  history = malloc(sizeof(List));
+  history->root = NULL;
+  return history;
 }
 
 void add_history(List* list, char* str)
 {
+  Item *new = malloc(sizeof(Item));
+  //if list is empty
+  if (list->root == NULL){
+    new->id = 1;
+    list->root = new;
+  }
+  else {
+    Item *currItem = list->root;
+    while(currItem->next != NULL)
+      {
+	currItem = currItem->next;
+      }
 
+    new->id = currItem->id + 1;
+    currItem->next = new;
+  }
+
+  new->str = str;
+  new->next = NULL;
 }
 
 char *get_history(List* list, int id)
 {
+  if (list->root == NULL)
+    {
+      return "List is empty";
+    }else
+    {
+      Item* temp = list->root;
 
+      while (temp != NULL)
+	{
+	  if (temp->id == id)
+	    return temp->str;
+	  temp = temp->next;
+	}
+      return "Item number not found\n";
+    }
 }
 
 void print_history(List* list)
